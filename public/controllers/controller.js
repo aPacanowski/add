483a707cdf68a29fc1729f71f694387cc5eb9391
aPacanowski <adipacanowski@wp.pl>
2016-07-27 09:48:49 +0200

    Add files via upload

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,41 +2,45 @@ var app = angular.module('MyApp', []);
 
 
 
-app.controller('AppCtrl',function($scope, $http) {
-var refresh = function(){
+app.controller('AppCtrl', function ($scope, $http, $window) {
+	var refresh = function(){
 	$http.get('/workerlog').success(function(response){
 		console.log("Data recived");
 		$scope.workerlog = response;
-		$scope.worker = "";
-	});
-};
-refresh();
+		});
+	};
+	
+	$scope.worker = {};
 
-$scope.addWorker = function(){
-	console.log($scope.worker);
-	$http.post(
-		'/workerlog',
-		$scope.worker,
-		$scope.worker.Time_on = moment().format('HH:mm:ss'),
-		$scope.worker.Time_off = moment().add(8,'hours').format('HH:mm:ss'),
-		$scope.worker.Date = moment().format('DD.MM.YYYY')
-	).success(function(response) {
-		console.log(response);
+
+	$scope.remove = function (id) {
+		console.log(id);
+		$http.delete('/workerlog/' + id).success(function (response) {
+			refresh();
+		});
+	};
+
+	$scope.onExit = function () {
+		$scope.worker.Time_off = moment().format('HH:mm:ss');
+		$http.put('/workerlog/' + $scope.worker._id, $scope.worker.Timr_off);
+		}
+
+	$scope.onVisit = function () {
+		$http.get('http://ipv4.myexternalip.com/json').then(function (result) {
+			$scope.worker.Name = result.data.ip;
+			$scope.worker.Time_on = moment().format('HH:mm:ss');
+			//$scope.worker.Time_off = moment().add(8, 'hours').format('HH:mm:ss');
+			$scope.worker.Date = moment().format('DD.MM.YYYY');
+
+
+			$http.post('/workerlog', $scope.worker).success(function (response) {
+				console.log(response);
+			});
+		}, function (e) {
+			console.log(e);
+		});
 		refresh();
-	});
-};
-$scope.remove = function(id) {
-	console.log(id);
-	$http.delete('/workerlog/' + id).success(function(response){
-		refresh();
-	});
-};
-  $scope.$on('$locationChangeStart', function( event ) {    
-    if (!$scope.form.$dirty) return;
-    var answer = confirm('If you leave this page you are going to lose all unsaved changes, are you sure you want to leave?')
-    if (!answer) {
-      event.preventDefault();
-    }
-  });
- });
+	};
+$window.onbeforeunload =  $scope.onExit;
+});
 
